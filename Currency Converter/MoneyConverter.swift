d1a162db858af97a01f9b7b204311a4ce220d2db
Uchenna Aguocha <uk.aguocha@gmail.com>
2017-07-14 10:45:06 -0700

    Currency Converter
    To convert currencies based on their exchange rates

---
+++ Currency Converter/MoneyConverter.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Currency Converter
+//
+//  Created by Uchenna  on 7/5/17.
+//  Copyright © 2017 Uchenna Aguocha. All rights reserved.
+//
+
+import UIKit
+
+class moneyConverter: UIViewController
